7d41748a4b0266905b9c5580d86c53246a4535b2
hdj0201 <40312210+hdj0201@users.noreply.github.com>
2019-03-22 09:56:31 +0900

    그림 수정,프론트 조정

--- France-bg.js
+++ France-bg.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 6;
+const IMG_NUMBER = 3;
 
 function handleImgLoad() {
     console.log("finished loading");
@@ -8,7 +8,7 @@ function handleImgLoad() {
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = `/follow-momentum/image/france/${imgNumber + 1}.jpg`;
+    image.src = `follow-momentum/image/france/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.appendChild(image);
     image.addEventListener("loadend", handleImgLoad);
